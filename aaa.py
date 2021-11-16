d6345e6c17e6a7e0782fbe3fe5a1d5a433390372
Omprasad <omprasadpoudel01@gmail.com>
2021-11-16 11:12:36 +0545

    first verson

---
+++ aaa.py
@@ -0,0 +1,11 @@
+import threading
+import os
+from tkinter import *
+
+root=Tk()
+root.geometry("800x600")
+root.title("Omprasad")
+label=Label(root, text="omprasad").pack()
+Button(root, text="clickme").place(x=100,y=200)
+
+root.mainloop()
